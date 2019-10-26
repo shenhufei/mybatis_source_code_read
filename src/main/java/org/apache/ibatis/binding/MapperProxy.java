2af1ce031eed3268acf1d78bcce2bfce40ed3747
shenhufei@aifunai.com <hufeishen@hufeidemacbook-air.local>
2019-10-26 21:52:45 +0800

    demo代码提交

---
+++ src/main/java/org/apache/ibatis/binding/MapperProxy.java
@@ -0,0 +1,96 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.binding;
+
+import java.io.Serializable;
+import java.lang.invoke.MethodHandles;
+import java.lang.invoke.MethodHandles.Lookup;
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.util.Map;
+
+import org.apache.ibatis.reflection.ExceptionUtil;
+import org.apache.ibatis.session.SqlSession;
+
+/**
+ * @author Clinton Begin
+ * @author Eduardo Macarron
+ */
+public class MapperProxy<T> implements InvocationHandler, Serializable {
+
+  private static final long serialVersionUID = -6424540398559729838L;
+  private static final int ALLOWED_MODES = MethodHandles.Lookup.PRIVATE | MethodHandles.Lookup.PROTECTED
+      | MethodHandles.Lookup.PACKAGE | MethodHandles.Lookup.PUBLIC;
+  private static Constructor<Lookup> lookupConstructor;
+  private final SqlSession sqlSession;
+  private final Class<T> mapperInterface;
+  private final Map<Method, MapperMethod> methodCache;
+
+  public MapperProxy(SqlSession sqlSession, Class<T> mapperInterface, Map<Method, MapperMethod> methodCache) {
+    this.sqlSession = sqlSession;
+    this.mapperInterface = mapperInterface;
+    this.methodCache = methodCache;
+  }
+
+  static {
+    try {
+      lookupConstructor = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class, int.class);
+    } catch (NoSuchMethodException e) {
+      try {
+        // Since Java 14+8
+        lookupConstructor = MethodHandles.Lookup.class.getDeclaredConstructor(Class.class, Class.class, int.class);
+      } catch (NoSuchMethodException e2) {
+        throw new IllegalStateException("No known constructor found in java.lang.invoke.MethodHandles.Lookup.", e2);
+      }
+    }
+    lookupConstructor.setAccessible(true);
+  }
+
+  @Override
+  public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+    try {
+    	//  mybatis 的执行的入口，从其他位置执行sql进来就是这里；
+      if (Object.class.equals(method.getDeclaringClass())) {
+        return method.invoke(this, args);
+      } else if (method.isDefault()) {
+        return invokeDefaultMethod(proxy, method, args);
+      }
+    } catch (Throwable t) {
+      throw ExceptionUtil.unwrapThrowable(t);
+    }
+    final MapperMethod mapperMethod = cachedMapperMethod(method);
+    //  分配去执行crud操作；
+    return mapperMethod.execute(sqlSession, args);
+  }
+
+  private MapperMethod cachedMapperMethod(Method method) {
+    return methodCache.computeIfAbsent(method, k -> new MapperMethod(mapperInterface, method, sqlSession.getConfiguration()));
+  }
+
+  private Object invokeDefaultMethod(Object proxy, Method method, Object[] args)
+      throws Throwable {
+    final Class<?> declaringClass = method.getDeclaringClass();
+    final Lookup lookup;
+    if (lookupConstructor.getParameterCount() == 2) {
+      lookup = lookupConstructor.newInstance(declaringClass, ALLOWED_MODES);
+    } else {
+      // SInce JDK 14+8
+      lookup = lookupConstructor.newInstance(declaringClass, null, ALLOWED_MODES);
+    }
+    return lookup.unreflectSpecial(method, declaringClass).bindTo(proxy).invokeWithArguments(args);
+  }
+}
---
+++ src/test/java/org/apache/ibatis/Amy/StartDemo.java
@@ -0,0 +1,40 @@
+/**
+ *    Copyright ${license.git.copyrightYears} the original author or authors.
+ *
+ *    Licensed under the Apache License, Version 2.0 (the "License");
+ *    you may not use this file except in compliance with the License.
+ *    You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *    Unless required by applicable law or agreed to in writing, software
+ *    distributed under the License is distributed on an "AS IS" BASIS,
+ *    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *    See the License for the specific language governing permissions and
+ *    limitations under the License.
+ */
+package org.apache.ibatis.Amy;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import org.apache.ibatis.datasource.pooled.PooledDataSource;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.logging.Log;
+import org.apache.ibatis.logging.LogFactory;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+public class StartDemo {
+	private static final Log log = LogFactory.getLog(StartDemo.class);
+	public static void main(String[] args) throws IOException {
+		String resource = "org/apache/ibatis/Amy/mybatis-config.xml";//"mybatis-config.xml"
+		InputStream inputStream = Resources.getResourceAsStream(resource);
+		SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+		SqlSession session = sqlSessionFactory.openSession();
+		User user = session.selectOne("org.apache.ibatis.Amy.UserMapper.selectUser",1L);
+		log.error(user.toString());
+	}
+
+}
