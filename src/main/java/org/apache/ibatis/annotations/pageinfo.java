66b25a10973bfce7c7bf8a1086f2f93f80fcbf2c
shenhufei@aifunai.com <hufeishen@hufeidemacbook-air.local>
2019-11-03 21:33:52 +0800

    对注解的解释

---
+++ src/main/java/org/apache/ibatis/annotations/pageinfo.java
@@ -0,0 +1,11 @@
+package org.apache.ibatis.annotations;
+
+/**
+ *   @Desc  该包下面的注解基本都是mybatis3.0之后注解开发的提供的注解
+ *   注解开发：没有xml配置文件，接口层，书写相关的sql语句，可以有“关联查询”，“动态sql”
+ *   @author shenhufei
+ *   @Date 2019年11月3日
+ */
+public class pageinfo {
+
+}
